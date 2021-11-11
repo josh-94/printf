afcd68f9c28d05e3ac479b7d5acdbe0128f4ed26
josh-94 <jeshua.cabanillas@gmail.com>
2021-11-11 18:02:57 -0500

    Adding main.h

--- main.h
+++ main.h
@@ -7,12 +7,12 @@
 typedef struct vars
 {
 	char op;
-	void (*f)(va_list, char *buff);
+	void (*f)(va_list, char **buff);
 } vars_t;
 int _printf(const char *format, ...);
-void p_string(va_list parameters, char *buff);
-void p_char(va_list parameters, char *buff);
- void match(char s,va_list parameters, char *buff);
+void p_string(va_list parameters, char **buff);
+void p_char(va_list parameters, char **buff);
+ void match(char s,va_list parameters, char **buff);
 int _puts(char *buffer, int size);
 
 #endif
